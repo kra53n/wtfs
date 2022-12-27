bc8aa37254b9a780a319337d5321c092415f9e65
Krai53n <greasha46@gmail.com>
2022-12-27 23:23:18 +0300

    add config example

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,9 @@ namespace fs = std::filesystem;
 int main(int argc, char** argv) {
 	fs::path path(fs::current_path());
 	std::cout << std::endl << path << std::endl;
+	for (auto file : fs::directory_iterator(path))
+	{
+		std::cout << std::endl << file;
+	}
 	return 0;
 }
